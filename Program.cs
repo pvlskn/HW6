c53c38c2fa5fb540a0c5c9b2addd9e718a42aa17
p p <pvlskn@yandex.ru>
2021-11-30 18:19:15 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Ввести с клавиатуры предложение. Предложение представляет собой слова, разделенные пробелом. Знаки препинания не используются. 
+ Найти самое длинное слово в строке.
+Определить является ли строка палиндромом без учёта пробелов и регистра*/
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите предложение без знаков препинания");
+        string Line = Console.ReadLine();
+        string[] Words = Line.Split(' ');
+        int MaxLenght = 0;
+        string LongestWord = "";
+
+        for (int i = 0; i < Words.Length; i++)
+
+            if (Words[i].Length > MaxLenght)
+            {
+                MaxLenght = Words[i].Length;
+                LongestWord = Words[i];
+            }
+        Console.WriteLine("{0} - самое длинное слово.", LongestWord);
+        /*string LongestWords = "";
+         for (int i = 0; i < Words.Length; i++) //если длинных слов больше одного, не работает(
+
+           if (Words[i].Length == MaxLenght && Words[i] != LongestWord)
+           {
+               LongestWords = LongestWord + " " + Words[i];
+           }*/
+        string LinWoS = Line.Replace(" ","");
+        char[] ReSt = LinWoS.ToCharArray();
+        Array.Reverse(ReSt);
+        string ReLinWoS = new string(ReSt);
+        if (LinWoS == ReLinWoS)
+        {
+            Console.WriteLine("Предложение - полиндром");
+        }
+        else
+        {
+            Console.WriteLine("Предложение - не полиндром");
+        }
+        Console.WriteLine("Нажмите любую клавишу для выхода.");
+        Console.ReadKey();
+    }
+}
